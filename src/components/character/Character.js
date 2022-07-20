dd8b850a425cc9bcc6fede7c615275d8799f8070
Ivan Mariic <imariic77@gmail.com>
2022-07-20 19:11:22 +0200

    WIP

--- src/components/character/Character.js
+++ src/components/character/Character.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { Container, Avatar } from "./Character.styles";
 import avatars from "../../assets";
+import { Container, Avatar, Title, Gradient } from "./Character.styles";
 
 const Character = () => {
   return (
     <Container>
       <Avatar source={avatars.hambi} />
+      <Gradient
+        start={{ x: 0.0, y: 0.25 }}
+        end={{ x: 0.5, y: 1.0 }}
+        locations={[0, 0.5, 0.6]}
+        colors={["#FFD700", "#FFC72C", "#FEBE10"]}
+      >
+        <Title>Hambi</Title>
+      </Gradient>
     </Container>
   );
 };
--- src/components/character/Character.styles.js
+++ src/components/character/Character.styles.js
@@ -1,11 +1,27 @@
+import LinearGradient from "react-native-linear-gradient";
 import styled from "styled-components";
 
-export const Container = styled.View`
-  width: 200px;
-  height: 300px;
+export const Container = styled.TouchableOpacity`
+  width: 300px;
+  height: 500px;
+  align-items: center;
 `;
 
 export const Avatar = styled.Image`
   width: 100%;
-  height: 100%;
+  height: 80%;
+`;
+
+export const Title = styled.Text`
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 32px;
+  margin-top: 16px;
+  color: rgba(7, 110, 247, 0.7);
+`;
+
+export const Gradient = styled(LinearGradient)`
+  width: 100%;
+  height: 20%;
+  align-items: center;
 `;
